2d7ba0c6bec389b6521175defa50e68fd8fc3a34
Nuno <joucknuckbr@gmail.com>
2021-10-09 12:05:10 -0300

    Salvando sepradamente todos atributos nos arrays

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const express = require('express');
-var product = require('./models/Product');
-var productDTO = new Array();
-var vendasDTO = new Array();
-var test;
-var dt;
+
+var listP = new Array();
+var listV = new Array();
+var listT = new Array();
+var listC = new Array();
+var listD = new Array();
 
 const app = express();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
@@ -17,25 +18,26 @@ app.get('/arquivos', (req, res) => {
         '/home/joucknuckbr/Área de trabalho/Braners/Intelitrader/quero-ser/Desafio/Caso de teste 2/c2_produtos.txt',
         '/home/joucknuckbr/Área de trabalho/Braners/Intelitrader/quero-ser/Desafio/Caso de teste 2/c2_vendas.txt'
     ];
-
+    
     const promises = [];
     for (const caminhoArquivo of caminhoArquivos) {
         promises.push(lerArquivo(caminhoArquivo));
     }
-
+    
     // readTextFile("file://home/joucknuckbr/Área de trabalho/Braners/Intelitrader/quero-ser/Desafio/Caso de teste 2/c2_vendas.txt");
-
+    
     Promise.all(promises).then(conteudoArquivos => {
         res
-            .set({ 'Content-Type': 'text/plain' })
-            .send(conteudoArquivos.join('\n'));
-            // productDTO.forEach((el, index)=>{
-            //     console.log(el+' indice: '+index);
-            //     console.log('fic');
-            // });
+        .set({ 'Content-Type': 'text/plain' })
+        .send(conteudoArquivos.join('\n'));
+        // productDTO.forEach((el, index)=>{
+        //     console.log(el+' indice: '+index);
+        //     console.log('fic');
+        // });
     }).catch(err => {
         res.status(500).send(err);
     });
+
 }); /* Final do GET /arquivos */
 
 function lerArquivo(caminhoArquivo) {
@@ -44,22 +46,62 @@ function lerArquivo(caminhoArquivo) {
     return new Promise((resolve, reject) => {
         console.log(caminhoArquivo, 3);
         
-        // readFile(caminhoArquivo, (err, data) /* callback */ => {
-        //     console.log(caminhoArquivo, 4, err ? 'Erro' : 'Sucesso');
-        //     err ? reject(err) : resolve(data);
-        // });
         fs.readFile(caminhoArquivo, 'utf-8', function(err, data){
             console.log(caminhoArquivo, 4, err ? 'Erro' : 'Sucesso');
-            // productDTO=conteudoArquivos.split("\n");
-            test=data.split("\n");
-            if(test[0].split(";").length - 1 == 3){
+            var test = data.split("\n");
+            
+            if(test[0].split(";").length - 1 == 2){
+
+                var productDTO = new Array();
                 productDTO=data.split("\n");
-                console.log(productDTO);
-                console.log(productDTO.length);
+
+                productDTO.forEach((el)=>{
+
+                    var product = new Object();
+                    var ind = new Array();
+                    var pos = 0;
+                    
+                    while ( pos != -1 ) {
+                        pos = el.indexOf(";", pos + 1);
+                        ind.push(pos);
+                    }
+                    
+                    product.codP = el.slice(0,-(el.length-5));
+                    product.qntdEstIniP = el.slice(6,-(el.length-ind[1]));
+                    product.qntdMinCo = el.slice(ind[1]+1);
+                    listP.push(product);
+                    
+                });
+                
+                listP.forEach((el,index)=>{
+                    console.log("cod: "+el.codP);
+                    console.log("qntd est: "+el.qntdEstIniP);
+                    console.log("qntd min: "+el.qntdMinCo);
+                });
+                
             }else{
+                
+                var vendasDTO = new Array();
                 vendasDTO=data.split("\n");
-                console.log(vendasDTO);
-                console.log(vendasDTO.length);
+
+                vendasDTO.forEach((el,index)=>{
+                    
+                    var sells = new Object();
+                    var ind = new Array();
+                    var pos = 0;
+                    
+                    while ( pos != -1 ) {
+                        pos = el.indexOf(";", pos + 1);
+                        ind.push(pos);
+                    }
+                    
+                    sells.codP = el.slice(0,-(el.length-5));
+                    sells.qntdV = el.slice(6,-(el.length-ind[1]));
+                    sells.sitV = el.slice(ind[1]+1,-(el.length-ind[2]));
+                    sells.qntdMinCo = el.slice(ind[2]+1);
+                    listV.push(sells);
+                    
+                });
             }
             err ? reject(err) : resolve(data);
         })
@@ -67,22 +109,8 @@ function lerArquivo(caminhoArquivo) {
     
 }
 
-// function readTextFile(file)
-// {
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.open("GET", file, false);
-//     rawFile.onreadystatechange = function ()
-//     {
-//         if(rawFile.readyState === 4)
-//         {
-//             if(rawFile.status === 200 || rawFile.status == 0)
-//             {
-//                 var allText = rawFile.responseText;
-//                 console.log(allText);
-//             }
-//         }
-//     }
-//     rawFile.send(null);
-// }
+function tranferencias(){
+    
+}
 
 app.listen(3000);
